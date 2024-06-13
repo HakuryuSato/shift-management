e470d29facc3b3baa8b6dd8206d9392cce7cb632
HakuryuSato <r700540@gmail.com>
2024-06-13 21:25:33 +0900

    依存関係の競合を解決

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+// pages/login.js
 
-const Page: React.FC = () => {
+"use client";
+
+import { useState, FormEvent} from 'react';
+
+export default function LoginPage() {
+  const [userName, setUserName] = useState('');
+  const [citePassword, setCitePassword] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // ログイン処理
+    console.log({ userName, citePassword });
+  };
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>本名:</label>
+        <input 
+          type="text" 
+          value={userName} 
+          onChange={(e) => setUserName(e.target.value)} 
+        />
+      </div>
+      <div>
+        <label>パスワード:</label>
+        <input 
+          type="password" 
+          value={citePassword} 
+          onChange={(e) => setCitePassword(e.target.value)} 
+        />
+      </div>
+      <button type="submit">ログイン</button>
+    </form>
   );
-};
-
-export default Page;
+}
