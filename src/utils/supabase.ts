8ed69ee1d2ffdb1e0a7074e61c23ab15b777f994
HakuryuSato <r700540@gmail.com>
2024-07-15 18:18:57 +0900

    不要なコメント削除

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-// import { Database } from '@types/supabasetype' // 型定義必要？
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
