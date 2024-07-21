9530ca9c7636cb57683c38ee8d9b9afe3f449f2e
HakuryuSato <r700540@gmail.com>
2024-07-21 15:57:07 +0900

    バックエンド化に伴う変更

--- src/utils/formatShiftsForTable.ts
+++ src/utils/formatShiftsForTable.ts
@@ -1,7 +1,8 @@
 import type InterFaceShiftQuery from '@customTypes/InterFaceShiftQuery';
 import type InterFaceAdminShiftTable from '@customTypes/InterFaceAdminShiftTable';
 
-export default function formatShiftsForTable(shifts: any[]): InterFaceAdminShiftTable[] {
+
+export default function formatShiftsForTable(shifts: any[]): InterFaceAdminShiftTable[] { // 注意！型変更したけど未チェック！
   return shifts.map((shift: any) => ({
     user_name: shift.user_name,
     start_time: shift.start_time,
