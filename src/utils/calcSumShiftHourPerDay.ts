cb2fa160fdfa8cd52c20e4f16656f5fe598bd9c2
HakuryuSato <r700540@gmail.com>
2024-07-26 21:09:55 +0900

    一旦色はシフト多すぎる日だけ

--- src/utils/calcSumShiftHourPerDay.ts
+++ src/utils/calcSumShiftHourPerDay.ts
@@ -21,7 +21,8 @@ export default function calcSumShiftHourPerDay(data: any[]) {
     const colorsByDate = Object.fromEntries(
         Object.entries(shiftHoursByDate).map(([date, value]) => [
             date,
-            value >= 70 ? 'bg-red' : value <= 56 ? 'bg-gray' : ''
+            // value >= 70 ? 'bg-red' : value <= 56 ? 'bg-orange' : ''
+            value >= 70 ? 'bg-orange' : ''
         ])
     );
 
