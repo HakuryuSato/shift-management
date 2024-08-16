29b53b5ec56c65e007e62ce837cb265dd2a3a174
HakuryuSato <r700540@gmail.com>
2024-08-16 20:25:24 +0900

    シフト編集モード追加のため

--- src/components/common/ShiftRegisterForm.tsx
+++ src/components/common/ShiftRegisterForm.tsx
@@ -11,11 +11,12 @@ import Button from "@ui/Button";
 import { getUserOptions, setUserOptions } from "@/utils/userOptions";
 import Modal from "@/components/common/Modal";
 import fetchUserData from "@/utils/fetchUserData";
+import ShiftEditToolBar from "@components/common/ShiftEditToolBar";
 
 // 型
 import type InterFaceShiftQuery from "@customTypes/InterFaceShiftQuery";
 
-type CommonShiftRegisterFormProps = {
+type ShiftRegisterFormProps = {
   isOpen: boolean;
   onClose: () => void;
   selectedDate: string | null;
@@ -26,7 +27,7 @@ type CommonShiftRegisterFormProps = {
   selectedEventShiftTime?: string | null;
 };
 
-const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
+const ShiftRegisterForm: React.FC<ShiftRegisterFormProps> = (
   {
     isOpen,
     onClose,
@@ -44,19 +45,21 @@ const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
   const [userId, setUserId] = useState<number>(user_id);
 
   const [startTime, setStartTime] = useState<string>(
-    selectedEventShiftTime ? selectedEventShiftTime.split("-")[0] : "08:30"
+    selectedEventShiftTime ? selectedEventShiftTime.split("-")[0] : "08:30",
   );
   const [endTime, setEndTime] = useState<string>(
-    selectedEventShiftTime ? selectedEventShiftTime.split("-")[1] : "18:00"
+    selectedEventShiftTime ? selectedEventShiftTime.split("-")[1] : "18:00",
   );
   const [userData, setUserData] = useState<
     { user_name: string; user_id: Number }[]
   >([]);
+  
+  
 
   useEffect(() => { // モーダル表示時にCookieから値取得してStateへ
     if (selectedShiftId && selectedEventShiftTime != null) { // もし選択シフトIDとイベントシフト時間がnullでないなら(編集モード)
-      setStartTime(selectedEventShiftTime.split("-")[0])
-      setEndTime(selectedEventShiftTime.split("-")[1])
+      setStartTime(selectedEventShiftTime.split("-")[0]);
+      setEndTime(selectedEventShiftTime.split("-")[1]);
     } else {
       const { start_time, end_time } = getUserOptions();
       setStartTime(start_time);
@@ -70,7 +73,9 @@ const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
         setUserId(Number(data[0].user_id));
       });
     }
-  }, []);
+    console.log("loop");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // エラー発生していた？
 
   // 関数------------------------------------------------------------
   const sendShiftData = async () => { // async 追加
@@ -96,16 +101,30 @@ const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
     onClose();
   };
 
+  const handleDelete = () => {
+    console.log();
+  };
+
   if (!isOpen) return null;
 
   return (
-    // ↓モーダル用の黒塗り背景
+    // モーダル用の黒塗り背景
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-bold">{selectedDate}</h2>
+      <div>
+        {/* もしシフトidがあるなら、編集モード用ツールバー */}
+        {selectedShiftId &&
+          (
+            <div>
+              <ShiftEditToolBar
+                onClose={handleDelete}
+                onEdit={handleDelete}
+                onDelete={handleDelete}
+              />
+            </div>
+          )}
       </div>
 
-      {selectedShiftId && <h3>編集モード</h3>}
+      <h2 className="text-lg mt-6 text-center">{selectedDate}</h2>
 
       {isAdmin && (
         <div className="mb-4">
@@ -132,10 +151,13 @@ const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
         </div>
       )}
 
+
+      {isRegi}
       <div className="p-4">
         <h3 className="mb-4 flex justify-center ">
           シフトを希望する時間を入力してください
         </h3>
+
         <div className="flex justify-center items-center space-x-2">
           <TimeInput
             initialValue={startTime}
@@ -159,4 +181,4 @@ const CommonShiftRegisterForm: React.FC<CommonShiftRegisterFormProps> = (
   );
 };
 
-export default CommonShiftRegisterForm;
+export default ShiftRegisterForm;
