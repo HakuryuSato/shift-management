67d37b30d4ce2b7749b675e836685433dee43fcc
HakuryuSato <r700540@gmail.com>
2024-09-15 22:04:46 +0900

    祝日を計算に追加

--- src/utils/createTableForAdminShift.ts
+++ src/utils/createTableForAdminShift.ts
@@ -1,6 +1,12 @@
-// Import required types
+// 基盤
+
+// 型
 import type InterFaceAdminShiftTable from '@customTypes/InterFaceAdminShiftTable';
 import type InterFaceTableUsers from "@customTypes/InterFaceTableUsers";
+
+// 変換用
+import toJapanDateString from './toJapanDateString';
+
 
 
 // メイン関数
@@ -8,31 +14,34 @@ export default function createTableForAdminShift(
   currentMonth: number,
   currentYear: number,
   formatedShifts: InterFaceAdminShiftTable[],
-  userNames: InterFaceTableUsers[]
+  userNames: InterFaceTableUsers[],
+  holidays: { title: string; date: string }[]
+
 ): (string | number)[][] {
+  // 関数ここから ---------------------------------------------------------------------------------------------------
+
   userNames.sort((a, b) => (a.user_name ?? '').localeCompare(b.user_name ?? ''));
-
-
 
   // 日付リストを生成（先月26日から今月25日まで）
   const dateList = generateDateListFrom26thTo25th(currentMonth, currentYear);
-  
-  const { userTotals, userOvertimeTotals } = calculateUserTotals(formatedShifts, userNames);
-  const dailyTotals = calculateDailyTotals(currentMonth, currentYear, formatedShifts);
-  
+
+
+  const { dailyTotals, userTotals, userOvertimeTotals } = calculateTotalHours(
+    formatedShifts,
+    userNames,
+    dateList,
+    holidays
+  );
+
   const headerRow = [`${currentMonth + 1}月`, "名前"].concat(userNames.map(user => user.user_name ?? ""));
   const overtimeRow = ["-", "時間外(H)"].concat(userOvertimeTotals.map(total => total.toString()));
   const totalRow = ["日付", "合計(H)"].concat(userTotals.map(total => total.toString()));
-
-
-
-
   const table: (string | number)[][] = [headerRow];
 
   dateList.forEach(date => {
     const row: (string | number)[] = [date, dailyTotals[date]];
     userNames.forEach(user => {
-      const shift = formatedShifts.find(shift => shift.user_name === user.user_name && shift.start_time && new Date(shift.start_time).toLocaleDateString('ja-JP') === date);
+      const shift = formatedShifts.find(shift => shift.user_name === user.user_name && shift.start_time && toJapanDateString(new Date(shift.start_time))=== date);
       if (shift && shift.start_time && shift.end_time) {
         row.push(`${formatTimeForJPHourMinute(shift.start_time)}-${formatTimeForJPHourMinute(shift.end_time)}`);
       } else {
@@ -42,13 +51,10 @@ export default function createTableForAdminShift(
     table.push(row);
   });
 
-  table.splice(1, 0, overtimeRow); 
+  table.splice(1, 0, overtimeRow);
   table.splice(2, 0, totalRow);
 
   const result = changeDateFormat(table);
-
-
-
 
 
   return result;
@@ -71,93 +77,81 @@ function calculateShiftHours(start: string, end: string): number {
 // 先月26日から今月25日までの日付リストを生成する関数
 function generateDateListFrom26thTo25th(currentMonth: number, currentYear: number): string[] {
   const dates = [];
-  
+
   const startDate = new Date(currentYear, currentMonth - 1, 26); // 先月26日
   const endDate = new Date(currentYear, currentMonth, 25); // 今月25日
 
   for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-    dates.push(new Date(date).toLocaleDateString('ja-JP'));
+    dates.push(toJapanDateString(new Date(date)));
   }
 
   return dates;
 }
 
-// Function to calculate total shift hours for each user
-function calculateUserTotals(
+// 日付の書式を変更("2024-07-01" => "7/1(月)")
+function changeDateFormat(input: any) {
+  const days = ['日', '月', '火', '水', '木', '金', '土'];
+  const output = [...input];
+  for (let i = 3; i < input.length; i++) {
+    const dateParts = input[i][0].split('-'); // "2024-07-01"を分割
+    const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2]));
+    const dayOfWeek = days[date.getDay()];
+    output[i][0] = `${date.getMonth() + 1}/${date.getDate()}(${dayOfWeek})`;
+  }
+  return output;
+}
+
+
+// 日別とユーザー別の合計労働時間を計算
+function calculateTotalHours(
   formatedShifts: InterFaceAdminShiftTable[],
-  userNames: InterFaceTableUsers[]
-): { userTotals: number[], userOvertimeTotals: number[] } {
+  userNames: InterFaceTableUsers[],
+  dateList: string[], // dateListを引数に追加
+  holidays: { title: string; date: string }[] // 祝日データを追加
+): { dailyTotals: Record<string, number>, userTotals: number[], userOvertimeTotals: number[] } {
   const userTotals = userNames.map(() => 0);
   const userOvertimeTotals = userNames.map(() => 0);
 
+  const dailyTotals: Record<string, number> = {};
+  dateList.forEach(date => { dailyTotals[date] = 0; });
+
+  const holidaySet = new Set(holidays.map(holiday => holiday.date));
+
   formatedShifts.forEach(shift => {
     if (shift.start_time && shift.end_time) {
+      const shiftDate = toJapanDateString(new Date(shift.start_time));
+      let dailyShiftHours = calculateShiftHours(
+        formatTimeForJPHourMinute(shift.start_time),
+        formatTimeForJPHourMinute(shift.end_time)
+      );
+
+
+
+      // ユーザーごとの合計に加算
       const userIndex = userNames.findIndex(user => user.user_name === shift.user_name);
       if (userIndex !== -1) {
-        let shiftHours = calculateShiftHours(
-          formatTimeForJPHourMinute(shift.start_time),
-          formatTimeForJPHourMinute(shift.end_time)
-        );
 
         // 昼休憩の時間を調整
         const startHour = new Date(shift.start_time).getHours();
         const endHour = new Date(shift.end_time).getHours();
         if (startHour <= 12 && endHour >= 13) {
-          shiftHours -= 1; // 1時間の昼休憩を減算
+          dailyShiftHours -= 1; // 1時間の昼休憩を減算
         }
 
-        // 8時間を超えた分は時間外として加算
-        if (shiftHours > 8) {
-          userOvertimeTotals[userIndex] += (shiftHours - 8); // 8時間を超える分を残業時間として計算
-          shiftHours = 8; // 残業時間を除いた8時間を通常時間として計算
+        // 日ごとの合計に加算
+        dailyTotals[shiftDate] += dailyShiftHours;
+
+        const isHoliday = holidaySet.has(shiftDate);
+        if (isHoliday) {
+          userOvertimeTotals[userIndex] += dailyShiftHours;
+        } else {
+          userOvertimeTotals[userIndex] += Math.max(0, dailyShiftHours - 8);
+          userTotals[userIndex] += Math.min(dailyShiftHours, 8);
         }
 
-        userTotals[userIndex] += shiftHours;
       }
     }
   });
 
-  return { userTotals, userOvertimeTotals };
+  return { dailyTotals, userTotals, userOvertimeTotals };
 }
-
-// Function to calculate total shift hours for each day
-function calculateDailyTotals(
-  currentMonth: number,
-  currentYear: number,
-  formatedShifts: InterFaceAdminShiftTable[]
-): Record<string, number> {
-  const dateList = generateDateListFrom26thTo25th(currentMonth, currentYear);
-  const dailyTotals: Record<string, number> = {};
-
-  dateList.forEach(date => {
-    dailyTotals[date] = 0;
-  });
-
-  formatedShifts.forEach(shift => {
-    if (shift.start_time && shift.end_time) {
-      const shiftDate = new Date(shift.start_time).toLocaleDateString('ja-JP');
-      const shiftHours = calculateShiftHours(
-        formatTimeForJPHourMinute(shift.start_time),
-        formatTimeForJPHourMinute(shift.end_time)
-      );
-      if (dailyTotals[shiftDate] !== undefined) {
-        dailyTotals[shiftDate] += shiftHours;
-      }
-    }
-  });
-
-  return dailyTotals;
-}
-
-// 日付の書式を変更("2024/07/01" => "7/1(月)"")
-function changeDateFormat(input:any) {
-  const days = ['日', '月', '火', '水', '木', '金', '土'];
-  const output = [...input];
-  for (let i = 3; i < input.length; i++) {
-      const date = new Date(input[i][0]);
-      const dayOfWeek = days[date.getDay()];
-      output[i][0] = `${date.getMonth() + 1}/${date.getDate()}(${dayOfWeek})`;
-  }
-  return output;
-}
-
