8464fbdf6fe0a431982ffd8049c6e8de5ddb116c
HakuryuSato <r700540@gmail.com>
2024-10-08 18:28:06 +0900

    自動登録用の型追加

--- src/customTypes/ApiResponses.ts
+++ src/customTypes/ApiResponses.ts
@@ -1,7 +1,7 @@
 import InterFaceShiftQuery from './InterFaceShiftQuery';
+import { AutoShiftSettings } from './AutoShiftTypes';
 
-
-
+// 汎用
 export interface SuccessResponse {
     message: string;
 }
@@ -11,6 +11,7 @@ export interface ErrorResponse {
 }
 
 
+// getShift
 export interface getShiftSuccessResponse {
     data: InterFaceShiftQuery[];
 }
@@ -19,6 +20,14 @@ export interface getShiftErrorResponse {
     error: string;
 }
 
+export interface FetchAutoShiftSettingsSuccessResponse {
+    data: AutoShiftSettings[];
+  }
+  
+  export interface FetchAutoShiftSettingsErrorResponse {
+    error: string;
+  }
 
+export type FetchAutoShiftSettingsAPIResponse = FetchAutoShiftSettingsSuccessResponse | FetchAutoShiftSettingsErrorResponse;
 export type AutoShiftSettingsAPIResponse = SuccessResponse | ErrorResponse;
 export type getShiftAPIResponse = getShiftSuccessResponse | getShiftErrorResponse;
