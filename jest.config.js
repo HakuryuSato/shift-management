ef350bdbb61f89824b2a98331ba311bb2af79ee3
HakuryuSato <r700540@gmail.com>
2024-10-11 12:41:15 +0900

    testEnvironment冗長のため削除

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
   preset: "ts-jest",
-  // testEnvironment: 'node',
+
 
   // tsconfig.jsonのパスエイリアスを継承
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
