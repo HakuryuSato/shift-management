0acb3c51d7e004f86e503e1bc505d9144202c888
HakuryuSato <r700540@gmail.com>
2024-10-11 12:36:55 +0900

    jest.config.js　ファイル名でtestEnvironmentを切り替え

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  // testEnvironment: 'node',
 
   // tsconfig.jsonのパスエイリアスを継承
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+
+  projects: [
+    {
+      testEnvironment: "node",
+      testMatch: ["**/*.server.test.ts"],
+    },
+    {
+      testEnvironment: "jsdom",
+      testMatch: ["**/*.client.test.ts"],
+    },
+  ],
 };
