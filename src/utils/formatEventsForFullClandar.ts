f78ced8d0a177cb26616187f178252eb241669e8
HakuryuSato <r700540@gmail.com>
2024-10-25 20:07:12 +0900

    utility関数以降準備

---
+++ src/utils/formatEventsForFullClandar.ts
@@ -0,0 +1,25 @@
+// // FullCalendar用にイベントを整形
+
+// import {}
+
+// export default function formatEventsForFullCalendar(data: any[]) {
+
+
+//     return data.map((shift) => ({
+//       id: String(shift.shift_id),
+//       start: shift.start_time,
+//       end: shift.end_time,
+//       // useUserNameAsTitleがtrueならshift.user_name、falseならshift.title、どちらも無ければ空文字
+//       title: isAllMembersView ? shift.user_name || '' : shift.title || '',
+//       display: 'block',
+  
+//       // backgroundColor: shift.is_approved ? 'green' : '',
+  
+//       extendedProps: {// カスタムプロパティを追加
+//         is_approved: shift.is_approved,
+//         user_name: shift.user_name,
+//         user_id: shift.user_id,
+//       },
+//     }));
+//   }
+  
