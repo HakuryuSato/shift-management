f4225ea1d9e3e94ada759dd416d6b1cc8e410d9e
HakuryuSato <r700540@gmail.com>
2024-10-29 21:26:42 +0900

    showとhideを廃止

--- src/stores/user/userHomeFABSlice.ts
+++ src/stores/user/userHomeFABSlice.ts
@@ -2,16 +2,14 @@ import { create } from 'zustand';
 
 interface UserHomeFABState {
   isUserHomeFABVisible: boolean;
+  setIsUserHomeFABVisible: (isVisible: boolean) => void;
   fabIconType: 'qr' | 'plus';
   setFABIconType: (type: 'qr' | 'plus') => void;
-  showUserHomeFAB: () => void;
-  hideUserHomeFAB: () => void;
 }
 
 export const useUserHomeFABStore = create<UserHomeFABState>((set) => ({
   isUserHomeFABVisible: true,
+  setIsUserHomeFABVisible: (isVisible) => set({ isUserHomeFABVisible: isVisible }),
   fabIconType: 'qr',
   setFABIconType: (type) => set({ fabIconType: type }),
-  showUserHomeFAB: () => set({ isUserHomeFABVisible: true }),
-  hideUserHomeFAB: () => set({ isUserHomeFABVisible: false }),
 }));
