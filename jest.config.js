c150dd9854f70e151518dc1ad9e710c9bb1bff97
HakuryuSato <r700540@gmail.com>
2024-11-03 13:03:01 +0900

    jestでsupabaseの環境変数を取得できるよう変更

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,31 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require("./tsconfig");
+const path = require('path');
 
-module.exports = {
+const commonConfig = {
   preset: "ts-jest",
-  // transform: {
-  //   '^.+\\.ts?$': 'ts-jest',
-  // },
-  // moduleFileExtensions: ['ts', 'js'],
-
-  // tsconfig.jsonのパスエイリアスを継承
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  // パスエイリアス継承
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/",
   }),
+  setupFiles: [path.resolve(__dirname, 'jest.setup.js')],
+};
 
+module.exports = {
   projects: [
     {
+      ...commonConfig,
+      displayName: 'server',
       testEnvironment: "node",
       testMatch: ["**/*.server.test.ts"],
     },
     {
+      ...commonConfig,
+      displayName: 'client',
       testEnvironment: "jsdom",
       testMatch: ["**/*.client.test.ts"],
     },
