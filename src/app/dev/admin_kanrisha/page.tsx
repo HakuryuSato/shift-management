a1e3f625a592a751bfeb8b14c228eabe168dfb78
HakuryuSato <r700540@gmail.com>
2024-11-04 20:07:47 +0900

    dev用ページ追加

---
+++ src/app/dev/admin_kanrisha/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Home = () => {
+  return (
+    <>
+      <b>管理者用ページ</b>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/app/dev/user/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { UserHome } from "@/components/user/UserHome";
+
+const Home = () => {
+  return (
+    <>
+      <UserHome />
+    </>
+  );
+};
+
+export default Home;
