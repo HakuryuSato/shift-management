4294b3828837ccb4a2326ca5812ae7c5d80f02d2
HakuryuSato <r700540@gmail.com>
2024-11-05 20:20:36 +0900

    loginページのロールがなくてもnpm run buildが中断されないよう修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,20 @@
 'use client'
+
 import LoginForm from "@/components/common/LoginForm";
-import { useRouter } from "next/router";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const LoginPage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <LoginComponent />
+        </Suspense>
+    );
+};
+
+const LoginComponent = () => {
     const searchParams = useSearchParams();
-    const role = (searchParams.get("role") as "user" | "admin") || "user";
+    const role = searchParams?.get("role") as "user" | "admin" || "user";
 
     return <LoginForm role={role} />;
 };
