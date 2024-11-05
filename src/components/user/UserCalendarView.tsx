5d361b1ee4c6f685c52adf66d414ebb3fa4305f4
HakuryuSato <r700540@gmail.com>
2024-11-05 19:34:38 +0900

    正社員の場合はモード切替ボタン非表示

--- src/components/user/UserCalendarView.tsx
+++ src/components/user/UserCalendarView.tsx
@@ -1,22 +1,23 @@
+"use client";
+
+// ライブラリ
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
+
+// コンポーネント
 import { CustomFullCalendar } from "@/components/common/CustomFullCalendar/CustomFullCalendar";
 import { CalendarViewToggle } from "@components/user/CalendarViewToggle";
-import { useUserCalendarViewStore } from "@stores/user/userCalendarViewSlice"; 
-import { useSession } from "next-auth/react";
-import type { CustomNextAuthUser } from "@/customTypes/CustomNextAuthUser";
 
-declare module "next-auth" {
-  interface Session {
-    user: CustomNextAuthUser;
-  }
-}
+// Store
+import { useUserCalendarViewStore } from "@stores/user/userCalendarViewSlice";
+import { useUserHomeStore } from "@/stores/user/userHomeSlice";
+
 
 export function UserCalendarView() {
-  const { isUserCalendarViewVisible } = useUserCalendarViewStore();
-  const { data: session } = useSession();
-  const employment_type = session?.user?.employment_type;
-
+  const isUserCalendarViewVisible = useUserCalendarViewStore((state) =>
+    state.isUserCalendarViewVisible
+  );
+  const employmentType = useUserHomeStore((state) => state.employmentType);
 
   if (!isUserCalendarViewVisible) {
     return null;
@@ -30,7 +31,8 @@ export function UserCalendarView() {
       justifyContent="center"
       style={{ minHeight: "auto" }}
     >
-      <CalendarViewToggle />
+      {/* 正社員は出退勤モードのみ */}
+      {employmentType !== "full_time" && <CalendarViewToggle />}
       <CustomFullCalendar />
     </Grid>
   );
