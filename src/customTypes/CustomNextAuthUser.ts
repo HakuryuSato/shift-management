26d1ed5e3fca1d2ab6c54afa498d3282b23139ec
HakuryuSato <r700540@gmail.com>
2024-11-05 19:52:42 +0900

    NextAuthのUser用の型からrole削除

--- src/customTypes/CustomNextAuthUser.ts
+++ src/customTypes/CustomNextAuthUser.ts
@@ -1,6 +1,9 @@
 import { User as NextAuthUser } from "next-auth";
 import { User as CustomUserType } from "@/customTypes/User";
 
-export interface CustomNextAuthUser extends NextAuthUser, CustomUserType {
-  role?: "user" | "admin";
-}
+// NextAuthUserに自前のUser型を結合
+export interface CustomNextAuthUser extends NextAuthUser, CustomUserType { }
+
+
+
+
