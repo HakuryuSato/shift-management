7448cd6280bac8c92dce271450118fbfaff777dc
HakuryuSato <r700540@gmail.com>
2024-11-07 11:49:44 +0900

    User選択用ドロップダウンコンポーネント追加

---
+++ src/components/common/Modal/UserDropdown.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+const UserDropdown: React.FC = () => (
+  <FormControl fullWidth>
+    <InputLabel>ユーザー名</InputLabel>
+    <Select>
+      <MenuItem value="User1">User1</MenuItem>
+      <MenuItem value="User2">User2</MenuItem>
+    </Select>
+  </FormControl>
+);
+
+export default UserDropdown;
