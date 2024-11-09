3ab77493e42daca92971ed12ed6020f6a39b51a4
HakuryuSato <r700540@gmail.com>
2024-11-09 19:46:05 +0900

    toJapanDateStringをデフォルトエクスポートではない

--- src/utils/toJapanDateString.ts
+++ src/utils/toJapanDateString.ts
@@ -1,9 +1,6 @@
-function toJapanDateString(date: Date = new Date()): string {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-  
-  export default toJapanDateString;
-  
+export const toJapanDateString = (date: Date = new Date()): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
