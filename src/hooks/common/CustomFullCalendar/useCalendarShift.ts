686084f110e42a0d727ae9f6d6c7d67cd8a92076
HakuryuSato <r700540@gmail.com>
2024-11-10 23:01:57 +0900

    月を切り替えれば同期するが、それ以外では同期できない

--- src/hooks/common/CustomFullCalendar/useCalendarShift.ts
+++ src/hooks/common/CustomFullCalendar/useCalendarShift.ts
@@ -1,6 +1,6 @@
 // ライブラリ
-import { useEffect } from 'react';
-import useSWR from 'swr';
+import { useEffect, useCallback } from 'react';
+import useSWR, { mutate } from 'swr';
 
 // Store
 import { useUserHomeStore } from '@/stores/user/userHomeSlice';
@@ -14,19 +14,37 @@ import { fetchShifts, fetchUsers } from '@/utils/client/apiClient';
 import { formatEventsForFullCalendar } from '@/utils/formatEventsForFullCalendar';
 // import { calcDateRangeForMonth } from '@/utils/calcDateRangeForMonth'; // shiftAPIの更新時に使う
 import calcSumShiftHourPerDay from "@utils/calcSumShiftHourPerDay";
+import { useModalContainer } from '../Modal/useModalContainer';
+
 
 
 //シフトの情報を取得し、個人用と全員用の状態にセットする
 export function useCalendarShift() {
-  const { setCustomFullCalendarAllMembersShiftEvents, customFullCalendarCurrentMonth,setCustomFullCalendarBgColorsPerDay } = useCustomFullCalendarStore();
+  // state
+  const customFullCalendarCurrentMonth = useCustomFullCalendarStore((state) => state.customFullCalendarCurrentMonth);
+  const customFullCalendarAllMembersShiftEvents = useCustomFullCalendarStore((state) => state.customFullCalendarAllMembersShiftEvents)
+  const isUserCalendarViewVisible = useUserCalendarViewStore((state) => state.isUserCalendarViewVisible);   // カレンダーView
+
+  // set
+  const setCustomFullCalendarAllMembersShiftEvents = useCustomFullCalendarStore((state) => state.setCustomFullCalendarAllMembersShiftEvents);
+  const setCustomFullCalendarBgColorsPerDay = useCustomFullCalendarStore((state) => state.setCustomFullCalendarBgColorsPerDay);
+
+  // // 更新
+  // const updateCustomFullCalendarEvents = useCustomFullCalendarStore((state) => state.updateCustomFullCalendarEvents);
+
+
+
+
 
   // shiftAPIの更新時、attendanceと同じように以下の形式で呼び出すように設計すること
   // const { start_date, end_date } = calcDateRangeForMonth(customFullCalendarCurrentMonth)
-  const { isUserCalendarViewVisible } = useUserCalendarViewStore();
 
+
+
+  // キャッシュ取得パターンの定義
   const { data: shifts } = useSWR(
     isUserCalendarViewVisible
-      ? ['AllMembers_shifts', '*', customFullCalendarCurrentMonth].join('-')
+      ? ['shifts', customFullCalendarCurrentMonth, customFullCalendarAllMembersShiftEvents].join('-')
       : null,
     () => fetchShifts({ user_id: '*', month: customFullCalendarCurrentMonth })
   );
@@ -39,16 +57,46 @@ export function useCalendarShift() {
   );
 
 
+  // カレンダーのシフトデータを更新(mutateでrevalidateを強制)
+  const updateCalendarShift = async () => {
 
-  useEffect(() => {
+    const shifts = await mutate(
+      ['shifts', customFullCalendarCurrentMonth, customFullCalendarAllMembersShiftEvents].join('-'));
+
+    // データを再取得
     if (shifts) {
-      
-      const formattedEvents = formatEventsForFullCalendar(shifts, users);      
+      const formattedEvents = formatEventsForFullCalendar(shifts, users);
+      console.log('formattedEvents', formattedEvents)
       setCustomFullCalendarAllMembersShiftEvents(formattedEvents);
 
       const calculatedShiftHoursData = calcSumShiftHourPerDay(shifts);
       setCustomFullCalendarBgColorsPerDay(calculatedShiftHoursData)
     }
-  }, [shifts, customFullCalendarCurrentMonth, isUserCalendarViewVisible, setCustomFullCalendarAllMembersShiftEvents, users, setCustomFullCalendarBgColorsPerDay]);
 
+
+  };
+
+
+
+
+  useEffect(() => { // 初回実行用
+    // カレンダーのシフトデータを更新
+    updateCalendarShift()
+
+  }, [])
+
+
+  // useEffect(() => {
+  //   if (shifts) {
+  //     console.log('Formatting events for FullCalendar');
+  //     const formattedEvents = formatEventsForFullCalendar(shifts, users);
+  //     console.log('formattedEvents', formattedEvents)
+  //     setCustomFullCalendarAllMembersShiftEvents(formattedEvents);
+
+  //     const calculatedShiftHoursData = calcSumShiftHourPerDay(shifts);
+  //     setCustomFullCalendarBgColorsPerDay(calculatedShiftHoursData)
+  //   }
+  // }, [shifts, customFullCalendarCurrentMonth, isUserCalendarViewVisible, customFullCalendarAllMembersShiftEvents, users]);
+
+  return { updateCalendarShift };
 }
--- src/hooks/common/Modal/useModalContainer.ts
+++ src/hooks/common/Modal/useModalContainer.ts
@@ -1,28 +1,70 @@
 import { useModalContainerStore } from "@/stores/common/modalContainerSlice"
+import { insertShift } from "@/app/actions/insertShift"
+import { useUserHomeStore } from "@/stores/user/userHomeSlice"
+import { useCustomFullCalendarStore } from "@/stores/common/customFullCalendarSlice"
+import { useModalContentStore } from "@/stores/common/modalContentSlice";
+import { toJapanISOString } from "@/utils/toJapanISOString";
+import { useCalendarShift } from "../CustomFullCalendar/useCalendarShift";
 
+import useSWR, { mutate } from 'swr';
 
 
 export const useModalContainer = () => {
-    const modalMode = useModalContainerStore((state) => state.modalMode)
-    const closeModal = useModalContainerStore((state) => state.closeModal)
-
+    const modalMode = useModalContainerStore((state) => state.modalMode);
+    const closeModal = useModalContainerStore((state) => state.closeModal);
+    const userId = useUserHomeStore((state) => state.userId);
+    const customFullCalendarRole = useCustomFullCalendarStore((state) => state.customFullCalendarRole);
+    const customFullCalendarSelectedDate = useCustomFullCalendarStore((state) => state.customFullCalendarClickedDate)
+    const modalContentSelectedStartTime = useModalContentStore((state) => state.modalContentSelectedStartTime)
+    const modalContentSelectedEndTime = useModalContentStore((state) => state.modalContentSelectedEndTime)
+    const {updateCalendarShift} = useCalendarShift()
     
-    const handleClickModalContainerButton = () => {
+
+    // 確認、保存、削除のテキストが入る親モーダルのボタン
+    const handleClickModalContainerButton = async() => {
         // ModalModeに応じて条件分岐、apiClientを呼び出して処理を行う
-        if (modalMode == 'confirm') {
-            closeModal()
+        if (modalMode === 'register') {
+            // ユーザーの場合
+            if (customFullCalendarRole === 'user') {
+                // 選択された日付と開始・終了時刻を組み合わせてISO形式の日時文字列を生成
+                const startDateTime = new Date(`${customFullCalendarSelectedDate?.dateStr}T${modalContentSelectedStartTime}:00`);
+                const endDateTime = new Date(`${customFullCalendarSelectedDate?.dateStr}T${modalContentSelectedEndTime}:00`);
 
-        } else if (modalMode == 'register') {
 
-        } else if (modalMode == 'delete') {
+                // Shift用のデータを作成して送信
+                await insertShift({
+                    user_id: userId,
+                    start_time: toJapanISOString(startDateTime),
+                    end_time: toJapanISOString(endDateTime)
+                });
 
-        } else if (modalMode == 'update') {
 
-        } else if (modalMode == 'multiple-register') {
+                // // await mutate()
+                // await mutate('/api/getShift', undefined, { revalidate: true })
 
+
+
+                
+            } else if (customFullCalendarRole === 'admin') { // 管理者の場合は、
+
+            }
+
+
+        } else if (modalMode === 'delete') {
+            // 削除処理をここに追加
+        } else if (modalMode === 'update') {
+            // 更新処理をここに追加
+        } else if (modalMode === 'multiple-register') {
+            // 複数登録の処理をここに追加
         }
 
-    }
+        // // 変更をカレンダーに反映
+        await updateCalendarShift();
 
-    return { handleClickModalContainerButton }
-}
+
+        // モーダルを閉じる
+        await closeModal();
+    };
+
+    return { handleClickModalContainerButton };
+};
