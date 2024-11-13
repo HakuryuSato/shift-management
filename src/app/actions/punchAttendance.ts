87e8981bb9f259835b53cf06738cb5eabc4d3294
HakuryuSato <r700540@gmail.com>
2024-11-13 12:36:24 +0900

    労働時間を計算してresultに格納するロジック追加

--- src/app/actions/punchAttendance.ts
+++ src/app/actions/punchAttendance.ts
@@ -1,15 +1,23 @@
 'use server';
 
+// サーバーアクション
 import { insertAttendanceStamp } from './insertAttendanceStamp';
 import { updateAttendanceStamp } from './updateAttendanceStamp';
+import { upsertAttendanceResult } from './upsertAttendanceResult';
+
+// utils
 import { getTimeRangeISOStrings, toJapanISOString } from '@/utils/common/dateUtils';
 import { handleSupabaseRequest } from '@/utils/server/handleSupabaseRequest';
+import { generateAttendanceResult } from '@/utils/server/generateAttendanceResult';
+
+// 型
 import type { AttendanceStamp } from '@/types/Attendance';
+
 
 /**
  * 出退勤の打刻を行うサーバーアクション
  * @param userId ユーザーID
- * @returns メッセージ文字列
+ * @returns AttendanceStamp
  */
 export async function punchAttendance(userId: number): Promise<AttendanceStamp[]> {
     const now = new Date();
@@ -17,7 +25,6 @@ export async function punchAttendance(userId: number): Promise<AttendanceStamp[]
 
     // 当日の時間範囲を取得
     const { startTimeISO, endTimeISO } = getTimeRangeISOStrings('day', now);
-
 
     // 当日の出勤記録を取得
     const data = await handleSupabaseRequest<AttendanceStamp[]>(async (supabase) => {
@@ -35,11 +42,18 @@ export async function punchAttendance(userId: number): Promise<AttendanceStamp[]
         const attendance_id = data[0]?.attendance_id;
 
         // end_timeを更新
-        return await updateAttendanceStamp(attendance_id, nowISO);
+        const attendanceStamp = await updateAttendanceStamp(attendance_id, nowISO);
 
-        // ここでAttendanceResultsを作成更新(awaitしないで)
+        // 非同期で時間を計算してresultテーブルにupsert
+        generateAttendanceResult(attendanceStamp[0])
+            .then(attendanceResult => {
+                upsertAttendanceResult(attendanceResult);
+            })
+            .catch(console.error); // エラーハンドリングを追加
 
-        
+        return attendanceStamp
+
+
     } else { // ないなら
         // 新しい出勤記録を作成
         return await insertAttendanceStamp(userId, nowISO);
