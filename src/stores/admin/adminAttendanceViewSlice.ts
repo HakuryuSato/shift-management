347403aa171d89244c8994c8b6e6e54609dfe572
HakuryuSato <r700540@gmail.com>
2024-11-14 14:19:59 +0900

    開始終了日用の状態追加

--- src/stores/admin/adminAttendanceViewSlice.ts
+++ src/stores/admin/adminAttendanceViewSlice.ts
@@ -3,16 +3,23 @@ import { create } from 'zustand';
 interface AdminAttendanceViewStore {
   isVisibleAdminAttendanceView: boolean;
   adminAttendanceViewCurrentDate: Date;
+  adminAttendanceViewStartDate: Date;
+  adminAttendanceViewEndDate: Date;
   showAdminAttendanceView: () => void;
   hideAdminAttendanceView: () => void;
   setAdminAttendanceViewDate: (date: Date) => void;
+  setAdminAttendanceViewDateRange: (startDate: Date, endDate: Date) => void;
 }
 
 export const useAdminAttendanceViewStore = create<AdminAttendanceViewStore>((set) => ({
   isVisibleAdminAttendanceView: false,
   adminAttendanceViewCurrentDate: new Date(),
+  adminAttendanceViewStartDate: new Date(),
+  adminAttendanceViewEndDate: new Date(),
 
   showAdminAttendanceView: () => set({ isVisibleAdminAttendanceView: true }),
   hideAdminAttendanceView: () => set({ isVisibleAdminAttendanceView: false }),
-  setAdminAttendanceViewDate: (date) => set({ adminAttendanceViewCurrentDate: date }), 
+  setAdminAttendanceViewDate: (date) => set({ adminAttendanceViewCurrentDate: date }),
+  setAdminAttendanceViewDateRange: (startDate, endDate) =>
+    set({ adminAttendanceViewStartDate: startDate, adminAttendanceViewEndDate: endDate }),
 }));
