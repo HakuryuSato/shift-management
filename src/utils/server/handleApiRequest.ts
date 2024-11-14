84e874dc528a718228132a0908383a6267a0b6aa
HakuryuSato <r700540@gmail.com>
2024-11-14 17:58:55 +0900

    supabaseのエラーをそのまま返すようhandleApiRequest変更

--- src/utils/server/handleApiRequest.ts
+++ src/utils/server/handleApiRequest.ts
@@ -15,14 +15,19 @@ export async function handleApiRequest<T>(handler: () => Promise<T>): Promise<Ne
     let status = 500;
 
     if (error instanceof Error) {
-      // 特定のエラーメッセージに基づいてステータスコードを設定
-      if (error.message === 'データが取得できませんでした') {
-        status = 404;
-        message = 'データが見つかりません';
-      } else {
-        message = '内部サーバーエラー';
-      }
+      // Supabaseのエラーメッセージをそのまま利用
+      message = error.message;
     }
+
+    // if (error instanceof Error) {
+    //   // 特定のエラーメッセージに基づいてステータスコードを設定
+    //   if (error.message === 'データが取得できませんでした') {
+    //     status = 404;
+    //     message = 'データが見つかりません';
+    //   } else {
+    //     message = '内部サーバーエラー';
+    //   }
+    // }
 
     return NextResponse.json({ error: message }, { status });
   }
