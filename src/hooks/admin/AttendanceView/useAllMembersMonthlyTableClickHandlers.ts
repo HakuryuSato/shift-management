15a52eda893d8f920f5918acbd0bc598c99277de
HakuryuSato <r700540@gmail.com>
2024-11-20 20:40:11 +0900

    ユーザーを選択したときにユーザー名が表示されるように変更

--- src/hooks/admin/AttendanceView/useAllMembersMonthlyTableClickHandlers.ts
+++ src/hooks/admin/AttendanceView/useAllMembersMonthlyTableClickHandlers.ts
@@ -1,12 +1,23 @@
 import { useAdminAttendanceViewStore } from '@/stores/admin/adminAttendanceViewSlice';
 import { useAdminHomeStore } from '@/stores/admin/adminHomeSlice';
+import { useAdminHomeTopBarStore } from '@/stores/admin/adminHomeTopBarSlice';
 import type { User } from '@/types/User';
 
+
 export function useAllMembersMonthlyTableClickHandlers() {
+  // Home
+  const setAdminHomeMode = useAdminHomeStore((state) => state.setAdminHomeMode)
+
+  // AttendanceView
   const hideAllMembersMonthlyTable = useAdminAttendanceViewStore((state) => state.hideAllMembersMonthlyTable);
   const showPersonalAttendanceTable = useAdminAttendanceViewStore((state) => state.showPersonalAttendanceTable);
   const setAdminAttendanceViewSelectedUser = useAdminAttendanceViewStore((state) => state.setAdminAttendanceViewSelectedUser);
-  const setAdminHomeMode = useAdminHomeStore((state) => state.setAdminHomeMode)
+
+
+  // TopBar
+  const hideAdminHomeTopBarUserEditButtons = useAdminHomeTopBarStore((state) => state.hideAdminHomeTopBarUserEditButtons)
+  const setAdminHomeTopBarTitleText = useAdminHomeTopBarStore((state) => state.setAdminHomeTopBarTitleText)
+
 
   // ユーザー名がクリックされたときのハンドラー
   const handleClickUserName = (user: User) => {
@@ -14,6 +25,9 @@ export function useAllMembersMonthlyTableClickHandlers() {
     setAdminHomeMode('PERSONAL_ATTENDANCE')
     hideAllMembersMonthlyTable();
     showPersonalAttendanceTable();
+    hideAdminHomeTopBarUserEditButtons();
+    setAdminHomeTopBarTitleText(`${user.user_name}`)
+
   };
 
   return {
--- src/hooks/admin/useAdminHomeTopBar.ts
+++ src/hooks/admin/useAdminHomeTopBar.ts
@@ -1,25 +1,36 @@
 import { useCallback } from 'react';
 import { useAdminHomeStore } from '@/stores/admin/adminHomeSlice';
 import { useAdminAttendanceViewStore } from '@/stores/admin/adminAttendanceViewSlice';
+import { useAdminHomeTopBarStore } from '@/stores/admin/adminHomeTopBarSlice';
+import { formatJapanDateToYearMonth } from '@/utils/common/dateUtils'
+
 
 export const useAdminAttendanceTopBar = () => {
     const adminHomeMode = useAdminHomeStore((state) => state.adminHomeMode);
     const setAdminHomeMode = useAdminHomeStore((state) => state.setAdminHomeMode);
     const hidePersonalAttendanceTable = useAdminAttendanceViewStore((state) => state.hidePersonalAttendanceTable);
     const showAllMembersMonthlyTable = useAdminAttendanceViewStore((state) => state.showAllMembersMonthlyTable);
+    const adminAttendanceViewEndDate = useAdminAttendanceViewStore((state) => state.adminAttendanceViewEndDate);
+    const showAdminHomeTopBarUserEditButtons = useAdminHomeTopBarStore((state) => state.showAdminHomeTopBarUserEditButtons);
+    const setAdminHomeTopBarTitleText = useAdminHomeTopBarStore((state) => state.setAdminHomeTopBarTitleText);
 
     const handleClickTopLeftButton = useCallback(() => {
         if (adminHomeMode === 'SHIFT') {
             setAdminHomeMode('MONTHLY_ATTENDANCE');
+            setAdminHomeTopBarTitleText(formatJapanDateToYearMonth(adminAttendanceViewEndDate));
+
         } else if (adminHomeMode === 'MONTHLY_ATTENDANCE') {
             setAdminHomeMode('SHIFT');
+            // ここで、フルカレの開始終了日をsetAdminHomeTopBarTitleTextにセット
+
         } else if (adminHomeMode === 'PERSONAL_ATTENDANCE') {
             setAdminHomeMode('MONTHLY_ATTENDANCE');
             hidePersonalAttendanceTable();
             showAllMembersMonthlyTable();
-
+            showAdminHomeTopBarUserEditButtons();
+            setAdminHomeTopBarTitleText(formatJapanDateToYearMonth(adminAttendanceViewEndDate));
         }
-    }, [adminHomeMode, hidePersonalAttendanceTable, setAdminHomeMode, showAllMembersMonthlyTable]);
+    }, [adminAttendanceViewEndDate, adminHomeMode, hidePersonalAttendanceTable, setAdminHomeMode, setAdminHomeTopBarTitleText, showAdminHomeTopBarUserEditButtons, showAllMembersMonthlyTable]);
 
     const handleClickUserRegister = useCallback(() => {
         console.log('ユーザー登録処理');
