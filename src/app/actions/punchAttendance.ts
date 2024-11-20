53107f345f335c6a872e4200fdacce0a21f767f1
HakuryuSato <r700540@gmail.com>
2024-11-20 20:10:23 +0900

    日付を打刻するよう変更

--- src/app/actions/punchAttendance.ts
+++ src/app/actions/punchAttendance.ts
@@ -5,7 +5,7 @@ import { insertAttendance } from './insertAttendance';
 import { updateAttendance } from './updateAttendance';
 
 // utils
-import { getTimeRangeISOStrings, toJapanISOString } from '@/utils/common/dateUtils';
+import { toJapanDateISOString, toJapanISOString } from '@/utils/common/dateUtils';
 import { handleSupabaseRequest } from '@/utils/server/handleSupabaseRequest';
 import { generateAttendanceWorkMinutes } from '@/utils/server/generateAttendanceWorkMinutes';
 
@@ -19,10 +19,8 @@ import type { Attendance } from '@/types/Attendance';
  */
 export async function punchAttendance(userId: number): Promise<Attendance[]> {
   const now = new Date();
-  const nowISO = toJapanISOString(now);
-
-  // 当日の時間範囲を取得
-  const { startTimeISO, endTimeISO } = getTimeRangeISOStrings('day', now);
+  const nowTime = toJapanISOString(now);
+  const today = toJapanDateISOString(now);
 
   // 当日の出勤記録を取得
   const existingAttendance = await handleSupabaseRequest<Attendance[]>(async (supabase) => {
@@ -30,20 +28,19 @@ export async function punchAttendance(userId: number): Promise<Attendance[]> {
       .from('attendances')
       .select('*')
       .eq('user_id', userId)
-      .gte('work_date', startTimeISO)
-      .lte('work_date', endTimeISO)
-      .order('stamp_start_time', { ascending: false })
+      .eq('work_date', today)
+      // .order('work_date', { ascending: false })
       .limit(1);
   });
 
-  if (existingAttendance && existingAttendance.length > 0) {
-    // 出勤データが既に存在する場合
+  // 戻り値があり、[]でなく、打刻開始があるなら
+  if (existingAttendance && existingAttendance.length > 0 && existingAttendance[0].stamp_start_time) {
     const attendanceId = existingAttendance[0].attendance_id;
 
-    // stamp_end_time を更新
+    // stamp_end_time を更新(ここでstartとendがそろっている想定になる)
     const updatedAttendance = await updateAttendance({
       attendance_id: attendanceId,
-      stamp_end_time: nowISO,
+      stamp_end_time: nowTime,
     });
 
     // 非同期で勤務時間や残業時間を計算し、データを更新
@@ -61,7 +58,8 @@ export async function punchAttendance(userId: number): Promise<Attendance[]> {
     // 出勤データが存在しない場合、新しいレコードを作成
     const newAttendance = await insertAttendance({
       user_id: userId,
-      stamp_start_time: nowISO,
+      work_date: today,
+      stamp_start_time: nowTime,
     });
 
     return newAttendance;
