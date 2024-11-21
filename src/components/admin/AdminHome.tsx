017efcb8990b9dd68f872bf5438150a946da3a73
HakuryuSato <r700540@gmail.com>
2024-11-21 17:44:38 +0900

    HomeのデータはHomeで取得するように

--- src/components/admin/AdminHome.tsx
+++ src/components/admin/AdminHome.tsx
@@ -2,7 +2,12 @@
 import { AdminAttendanceView } from "./AttendanceView/AdminAttendanceView";
 import { AdminHomeTopBar } from "./AdminHomeTopBar";
 
+import { useAdminHomeUsersData } from "@/hooks/admin/useAdminHomeUsersData";
+
 export const AdminHome: React.FC = () => {
+    // ユーザー情報を取得するカスタムフックを呼び出す
+    useAdminHomeUsersData();
+    
     return (
         <>
             <AdminHomeTopBar />
