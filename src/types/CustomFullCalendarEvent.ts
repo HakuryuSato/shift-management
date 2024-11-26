2778706ee640702d690d0dc7aacbacb2c6bf6c70
HakuryuSato <r700540@gmail.com>
2024-11-26 19:06:03 +0900

    型の注釈追加

--- src/types/CustomFullCalendarEvent.ts
+++ src/types/CustomFullCalendarEvent.ts
@@ -1,5 +1,19 @@
 import { EventInput } from '@fullcalendar/core';
 
+/**
+ *  実際に使用している内容(src\utils\client\formatEventsForFullCalendar.ts)
+ * id: String(id),
+    start: record.start_time,
+    end: endTime,
+    title: user_name,
+    display: 'block',
+    extendedProps: {
+      user_id: record.user_id,
+      user_name: user_name,
+    },
+ * 
+ */
+
 export interface CustomFullCalendarEvent extends EventInput {
   extendedProps?: {
     user_id?: number;
