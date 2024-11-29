4b97bbac45c25108ed7b3efd11a207b6c37e4905
HakuryuSato <r700540@gmail.com>
2024-11-29 18:30:55 +0900

    状態名を具体的に変更、グローバル化が不要な状態はuseStateへ変更

--- src/stores/admin/adminUserManagementFormSlice.ts
+++ src/stores/admin/adminUserManagementFormSlice.ts
@@ -1,32 +1,21 @@
-
 import { create } from "zustand";
 
 interface AdminUserManagementFormState {
-  isAdminUserManagementFormVisible: boolean;
-  mode: "register" | "delete";
-  userName: string;
-  employmentType: "full_time" | "part_time";
+  isVisibleAdminUserManagementForm: boolean;
+  adminUserManagementFormMode: "register" | "delete";
   openAdminUserManagementForm: (mode: "register" | "delete") => void;
   closeAdminUserManagementForm: () => void;
-  setUserName: (userName: string) => void;
-  setEmploymentType: (employmentType: "full_time" | "part_time") => void;
 }
 
 export const useAdminUserManagementFormStore = create<AdminUserManagementFormState>(
   (set) => ({
-    isAdminUserManagementFormVisible: false,
-    mode: "register",
-    userName: "",
-    employmentType: "full_time",
+    isVisibleAdminUserManagementForm: false,
+    adminUserManagementFormMode: "register",
     openAdminUserManagementForm: (mode) =>
-      set({ isAdminUserManagementFormVisible: true, mode }),
+      set({ isVisibleAdminUserManagementForm: true, adminUserManagementFormMode: mode }),
     closeAdminUserManagementForm: () =>
       set({
-        isAdminUserManagementFormVisible: false,
-        userName: "",
-        employmentType: "full_time",
+        isVisibleAdminUserManagementForm: false,
       }),
-    setUserName: (userName) => set({ userName }),
-    setEmploymentType: (employmentType) => set({ employmentType }),
   })
 );
