a738fe80c08d0f8175af82804dd5a12a4409f3c9
HakuryuSato <r700540@gmail.com>
2024-11-29 17:37:14 +0900

    Home初期化関数の実行を追加

--- src/components/admin/AdminHome.tsx
+++ src/components/admin/AdminHome.tsx
@@ -1,16 +1,23 @@
 "use client";
+// Component
 import { AdminAttendanceView } from "./AttendanceView/AdminAttendanceView";
 import { AdminHomeTopBar } from "./AdminHomeTopBar";
+import { ModalContainer } from "../common/Modal/ModalContainer";
 
+
+// Hooks
+import { useCommonHomeInitialize } from "@/hooks/user/useCommonHomeInitialize";
 import { useAdminHomeUsersData } from "@/hooks/admin/useAdminHomeUsersData";
 
 export const AdminHome: React.FC = () => {
     // ユーザー情報を取得するカスタムフックを呼び出す
     useAdminHomeUsersData();
-    
+    useCommonHomeInitialize('admin')
+
     return (
         <>
             <AdminHomeTopBar />
+            <ModalContainer />
             <AdminAttendanceView />
         </>
     );
