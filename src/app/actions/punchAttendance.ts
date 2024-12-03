258cff332715d2219ade7b0f6e9c99c5628cad81
HakuryuSato <r700540@gmail.com>
2024-12-03 16:02:51 +0900

    デバッグ用ログ追加

--- src/app/actions/punchAttendance.ts
+++ src/app/actions/punchAttendance.ts
@@ -18,10 +18,12 @@ import type { Attendance } from '@/types/Attendance';
  * @returns 更新または作成された Attendance データ
  */
 export async function punchAttendance(userId: number): Promise<Attendance[]> {
+  console.log(`[${new Date().toISOString()}] punchAttendance 開始`);
   const now = new Date();
   const nowTime = toJapanISOString(now);
   const today = toJapanDateISOString(now);
 
+  console.log(`[${new Date().toISOString()}] punchAttendance existingAttendance 取得`);
   // 当日の出勤記録を取得
   const existingAttendance = await handleSupabaseRequest<Attendance[]>(async (supabase) => {
     return supabase
@@ -37,24 +39,31 @@ export async function punchAttendance(userId: number): Promise<Attendance[]> {
   if (existingAttendance && existingAttendance.length > 0 && existingAttendance[0].stamp_start_time) {
     const attendanceId = existingAttendance[0].attendance_id;
 
+    console.log(`[${new Date().toISOString()}] punchAttendance updateAttendance 開始`);
     // stamp_end_time を更新(ここでstartとendがそろっている想定になる)
     const updatedAttendance = await updateAttendance({
       attendance_id: attendanceId,
       stamp_end_time: nowTime,
     });
+    console.log(`[${new Date().toISOString()}] punchAttendance updateAttendance 終了、generateAttendanceWorkMinutes開始`);
+
 
     // 非同期で勤務時間や残業時間を計算し、データを更新
     generateAttendanceWorkMinutes(updatedAttendance[0])
       .then(async (result) => {
+        console.log(`[${new Date().toISOString()}] punchAttendance generateAttendanceWorkMinutes終了, updateAttendance(result) 開始 `);
         await updateAttendance(result);
+        console.log(`[${new Date().toISOString()}] punchAttendance updateAttendance(result) 終了 `);
       })
       .catch((error) => {
         console.error('Error generating attendance result:', error);
         throw new Error('Failed to calculate attendance result.');
       });
-
+    
+      console.log(`[${new Date().toISOString()}] punchAttendance 終了`);
     return updatedAttendance;
   } else {
+    console.log(`[${new Date().toISOString()}] punchAttendance insertAttendance 開始`);
     // 出勤データが存在しない場合、新しいレコードを作成
     const newAttendance = await insertAttendance({
       user_id: userId,
@@ -62,6 +71,7 @@ export async function punchAttendance(userId: number): Promise<Attendance[]> {
       stamp_start_time: nowTime,
     });
 
+    console.log(`[${new Date().toISOString()}] punchAttendance 終了 insertAttendance 終了`);
     return newAttendance;
   }
 }
