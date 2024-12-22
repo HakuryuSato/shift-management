936dab37535cb910c011ac074d811511d0b4d315
HakuryuSato <r700540@gmail.com>
2024-12-22 20:34:15 +0900

    userの参照先を変更

---
+++ src/app/user/old_page.tsx
@@ -0,0 +1,51 @@
+// "use client";
+
+// // 基盤
+// import { useEffect, useState } from "react";
+// import Cookies from "js-cookie";
+
+
+// // 独自
+// import DayGridCalendar from "@forms/DayGridCalendar";
+// import UserLoginForm from "@components/forms/UserLoginForm";
+
+// // 型
+// import type InterFaceTableUsers from "@/types/InterFaceTableUsers";
+
+// // クッキー名称
+// const COOKIE_USER_LOGGED_IN = process.env
+//   .NEXT_PUBLIC_COOKIE_USER_LOGGEDIN as string;
+// const COOKIE_USER_INFO = process.env.NEXT_PUBLIC_COOKIE_USER_INFO as string;
+// const COOKIE_USER_OPTIONS = process.env
+//   .NEXT_PUBLIC_COOKIE_USER_OPTIONS as string;
+
+// export default function UserPage() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [user, setUser] = useState<InterFaceTableUsers>({
+//     user_id: 0,
+//     user_name: "",
+//   });
+
+//   useEffect(() => { // ページ表示時にログイン状態とクッキーを確認
+//     const loggedIn = Cookies.get(COOKIE_USER_LOGGED_IN);
+//     const userInfo = Cookies.get(COOKIE_USER_INFO);
+
+//     setIsLoggedIn(!!loggedIn);
+//     if (userInfo) { // クッキーにユーザー情報があるなら
+//       setUser(JSON.parse(userInfo)); // クッキーからユーザー情報を取得
+//     }
+//   }, []);
+
+//   const handleLoginSuccess = (userData: InterFaceTableUsers) => { // ログイン成功時にユーザー情報をクッキーに保存
+//     setIsLoggedIn(true);
+//     setUser(userData);
+//   };
+
+//   return (
+//     <>
+//       {isLoggedIn // ログイン済みならカレンダー、まだならログインフォーム
+//         ? <DayGridCalendar user={user} />
+//         : <UserLoginForm onLoginSuccess={handleLoginSuccess} />}
+//     </>
+//   );
+// }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,51 +1,14 @@
 "use client";
 
-// 基盤
-import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
+import { UserHome } from "@/components/user/UserHome";
+import { SessionProvider } from "next-auth/react";
 
-
-// 独自
-import DayGridCalendar from "@forms/DayGridCalendar";
-import UserLoginForm from "@components/forms/UserLoginForm";
-
-// 型
-import type InterFaceTableUsers from "@/types/InterFaceTableUsers";
-
-// クッキー名称
-const COOKIE_USER_LOGGED_IN = process.env
-  .NEXT_PUBLIC_COOKIE_USER_LOGGEDIN as string;
-const COOKIE_USER_INFO = process.env.NEXT_PUBLIC_COOKIE_USER_INFO as string;
-const COOKIE_USER_OPTIONS = process.env
-  .NEXT_PUBLIC_COOKIE_USER_OPTIONS as string;
-
-export default function UserPage() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState<InterFaceTableUsers>({
-    user_id: 0,
-    user_name: "",
-  });
-
-  useEffect(() => { // ページ表示時にログイン状態とクッキーを確認
-    const loggedIn = Cookies.get(COOKIE_USER_LOGGED_IN);
-    const userInfo = Cookies.get(COOKIE_USER_INFO);
-
-    setIsLoggedIn(!!loggedIn);
-    if (userInfo) { // クッキーにユーザー情報があるなら
-      setUser(JSON.parse(userInfo)); // クッキーからユーザー情報を取得
-    }
-  }, []);
-
-  const handleLoginSuccess = (userData: InterFaceTableUsers) => { // ログイン成功時にユーザー情報をクッキーに保存
-    setIsLoggedIn(true);
-    setUser(userData);
-  };
-
+const Home = () => {
   return (
-    <>
-      {isLoggedIn // ログイン済みならカレンダー、まだならログインフォーム
-        ? <DayGridCalendar user={user} />
-        : <UserLoginForm onLoginSuccess={handleLoginSuccess} />}
-    </>
+    <SessionProvider>
+      <UserHome />
+    </SessionProvider>
   );
-}
+};
+
+export default Home;
