6c0d1c913406480405ce63001c03c3b74fba433e
HakuryuSato <r700540@gmail.com>
2025-01-22 21:09:45 +0900

    フルカレrefのset条件を値がない場合に変更

--- src/components/common/CustomFullCalendar/CustomFullCalendar.tsx
+++ src/components/common/CustomFullCalendar/CustomFullCalendar.tsx
@@ -68,7 +68,7 @@ export function CustomFullCalendar() {
     <div {...reactSwipeHandlers}>
       <FullCalendar
         ref={(ref) => {
-          if (ref) {
+          if (ref && ref !== customFullCalendarRef) {
             setCustomFullCalendarRef(ref);
           }
         }}
