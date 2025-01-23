a462b7ac805335d87ce3bd3e851fb578a90e4d77
HakuryuSato <r700540@gmail.com>
2025-01-23 18:03:15 +0900

    admin shift viewを追加

--- src/components/admin/AdminHome.tsx
+++ src/components/admin/AdminHome.tsx
@@ -4,7 +4,7 @@ import { AdminAttendanceView } from "./AttendanceView/AdminAttendanceView";
 import { AdminHomeTopBar } from "./AdminHomeTopBar";
 import { ModalContainer } from "../common/Modal/ModalContainer";
 import { AdminUserManagementForm } from "./AttendanceView/AdminUserManagementForm";
-
+import { AdminShiftView } from "./AdminShiftView";
 
 // Hooks
 import { useCommonHomeInitialize } from "@/hooks/user/useCommonHomeInitialize";
@@ -13,14 +13,16 @@ import { useAdminHomeUsersData } from "@/hooks/admin/useAdminHomeUsersData";
 export const AdminHome: React.FC = () => {
     // ユーザー情報を取得するカスタムフックを呼び出す
     useAdminHomeUsersData();
-    useCommonHomeInitialize('admin')
+    useCommonHomeInitialize("admin");
 
     return (
         <>
             <AdminHomeTopBar />
-            <AdminUserManagementForm/>
+            <AdminUserManagementForm />
             <ModalContainer />
+            
             <AdminAttendanceView />
+            <AdminShiftView />
         </>
     );
 };
