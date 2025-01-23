9dd029a7ab6734c8a35a5811c576e3dc2153b3e3
HakuryuSato <r700540@gmail.com>
2025-01-23 18:52:10 +0900

    admin_kanrishaのページにAdminHomeを追加

--- src/app/admin_kanrisha/page.tsx
+++ src/app/admin_kanrisha/page.tsx
@@ -1,63 +1,14 @@
 "use client";
-// 基盤
-import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
 
-// 独自
-// import AdminSelectPage from "@forms/AdminSelectPage";
-import AdminShiftTable from "@forms/AdminShiftTable"
-import AdminLoginForm from "@components/forms/AdminLoginForm";
-import TimeGridCalendar from "@forms/TimeGridCalendar";
+import { AdminHome } from "@/components/admin/AdminHome";
+import { SessionProvider } from "next-auth/react";
 
-const COOKIE_ADMIN_ISLOGGEDIN = process.env.NEXT_PUBLIC_COOKIE_ADMIN_ISLOGGEDIN as string
-
-
-export default function AdminPage() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [currentPage, setCurrentPage] = useState("AdminSelectPage");
-
-  // フック
-  useEffect(() => { // ページ表示時にログイン状態かクッキーを確認
-    const loggedIn = Cookies.get(COOKIE_ADMIN_ISLOGGEDIN);
-    setIsLoggedIn(!!loggedIn);
-  }, []);
-
-  // ハンドラ
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => { // デバッグ用 ログアウト
-    Cookies.remove(COOKIE_ADMIN_ISLOGGEDIN);
-    setIsLoggedIn(false);
-  };
-
-  const handleBackButtonClick = (page: string) => {
-    if (page === "Logout") {
-      handleLogout();
-    } else {
-      setCurrentPage(page);
-    }
-  };
-
+const Home = () => {
   return (
-    <>
-      {isLoggedIn
-        ? (
-          currentPage === "AdminShiftTable"
-            ? (
-              <AdminShiftTable
-              onButtonClickBackToShiftApproval={() => handleBackButtonClick("TimeGridCalendar")}
-              />
-            )
-            : (
-              <TimeGridCalendar
-                onLogout={handleLogout}
-                onBack={() => handleBackButtonClick("AdminShiftTable")}
-              />
-            )
-        )
-        : <AdminLoginForm onLoginSuccess={handleLoginSuccess} />}
-    </>
+    <SessionProvider>
+      <AdminHome />
+    </SessionProvider>
   );
-}
+};
+
+export default Home;
