a882bb6a1fe75a5c40855423c75d451b04180b50
HakuryuSato <r700540@gmail.com>
2025-01-26 12:05:13 +0900

    UserDropDown 横幅修正

--- src/components/common/Modal/UserDropdown.tsx
+++ src/components/common/Modal/UserDropdown.tsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React, { useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { useAdminHomeStore } from "@/stores/admin/adminHomeSlice";
 
 export const UserDropdown: React.FC = () => {
   const adminHomeUsersData = useAdminHomeStore((state) =>
     state.adminHomeUsersData
   );
+  const [selectedUser, setSelectedUser] = useState<string>("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedUser(event.target.value);
+  };
 
   return (
     <FormControl fullWidth>
       <InputLabel>ユーザー名</InputLabel>
-      <Select>
+      <Select
+        value={selectedUser}
+        onChange={handleChange}
+        label="ユーザー名"
+        sx={{ minWidth: '150px' }}
+      >
         {adminHomeUsersData?.map((user, index) => (
           <MenuItem key={index} value={user.user_name}>
             {user.user_name}
