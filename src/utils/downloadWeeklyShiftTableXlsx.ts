39fcf9a4f7b8c1605b30d58f03acf1745bf8948c
HakuryuSato <r700540@gmail.com>
2025-01-26 17:25:48 +0900

    関数名にweeklyを追加

--- src/utils/downloadWeeklyShiftTableXlsx.ts
+++ src/utils/downloadWeeklyShiftTableXlsx.ts
@@ -17,7 +17,7 @@ type ShiftEvent = {
 };
 
 // メイン関数
-export const downloadShiftTableXlsx = async (startDate: Date, endDate: Date, shiftEvents: any[]) => {
+export const downloadWeeklyShiftTableXlsx = async (startDate: Date, endDate: Date, shiftEvents: any[]) => {
   // 表示用文字列生成 MM月DD日~MM月DD日
   const fileName = `週間シフト表_${startDate.getMonth() + 1}月${startDate.getDate()}日 ~ ${
     endDate.getMonth() + 1
