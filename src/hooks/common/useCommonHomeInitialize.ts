ee8552d31b7f3dfad47e4e285b5332cdf586c605
HakuryuSato <r700540@gmail.com>
2025-01-26 10:52:50 +0900

    Home初期化時、管理者はフルカレのモードを全員シフトに変更

--- src/hooks/common/useCommonHomeInitialize.ts
+++ src/hooks/common/useCommonHomeInitialize.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useUserHomeUserSession } from '@/hooks/user/useUserHomeUserSession';
 import { useModalContainerStore } from '@/stores/common/modalContainerSlice';
 import { useCustomFullCalendarStore } from '@/stores/common/customFullCalendarSlice';
+import { useCalendarViewToggleStore } from '@/stores/user/calendarViewToggleSlice';
 
 export function useCommonHomeInitialize(role: 'user' | 'admin') {
     useUserHomeUserSession();
@@ -9,6 +10,16 @@ export function useCommonHomeInitialize(role: 'user' | 'admin') {
     // 状態更新関数を取得
     const setCustomFullCalendarRole = useCustomFullCalendarStore((state) => state.setCustomFullCalendarRole);
     const setModalRole = useModalContainerStore((state) => state.setModalRole);
+
+    const setCalendarViewModeToAllMembersShift = useCalendarViewToggleStore(
+        (state) => state.setCalendarViewModeToAllMembersShift
+    );
+
+
+    if (role === 'admin') {
+        setCalendarViewModeToAllMembersShift();
+    }
+
 
     useEffect(() => {
         setCustomFullCalendarRole(role);
