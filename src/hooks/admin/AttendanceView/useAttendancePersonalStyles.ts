39509c35257c4137bb0ec19ace9ec14ce47d9f74
HakuryuSato <r700540@gmail.com>
2025-02-16 17:59:40 +0900

    usePersonalStylesについて、日付範囲をstoreから取得するよう修正

--- src/hooks/admin/AttendanceView/useAttendancePersonalStyles.ts
+++ src/hooks/admin/AttendanceView/useAttendancePersonalStyles.ts
@@ -6,13 +6,23 @@ import { alpha } from '@mui/material';
 export const useAttendancePersonalStyles = () => {
   const theme = useTheme();
   const holidays = useAdminAttendanceViewStore(state => state.adminAttendanceViewHolidaysMap);
+  const startDate = useAdminAttendanceViewStore(state => state.adminAttendanceViewStartDate);
+  const endDate = useAdminAttendanceViewStore(state => state.adminAttendanceViewEndDate);
   const setAttendanceTablePersonalRowStyles = useAttendanceTablePersonalStore(
     state => state.setAttendanceTablePersonalRowStyles
   );
 
   const holidayBackgroundColor = alpha(theme.palette.error.light, 0.1);
 
-  const updateAttendancePersonalRowStyles = (dates: string[]) => {
+  const updateAttendancePersonalRowStyles = () => {
+    // 日付の配列を生成
+    const dates: string[] = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+      dates.push(currentDate.toISOString().split('T')[0]);
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
     const styles: { [key: string]: { backgroundColor: string } } = {};
     dates.forEach(date => {
       const isHoliday = holidays?.has(date);
