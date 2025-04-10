65286840101857655a56b7eacc0051c2027e9a23
HakuryuSato <r700540@gmail.com>
2025-04-10 14:45:48 +0900

    ClosingDateFormSliceからclosing-dateを削除、attendanceViewで管理するため

--- src/stores/admin/adminClosingDateFormSlice.ts
+++ src/stores/admin/adminClosingDateFormSlice.ts
@@ -4,14 +4,10 @@ interface AdminClosingDateFormState {
   isVisibleAdminClosingDateForm: boolean;
   openAdminClosingDateForm: () => void;
   closeAdminClosingDateForm: () => void;
-  adminClosingDateFormDate: string;
-  setAdminClosingDateFormDate: (date: string) => void;
 }
 
 export const useAdminClosingDateFormStore = create<AdminClosingDateFormState>((set) => ({
   isVisibleAdminClosingDateForm: false,
-  adminClosingDateFormDate: '',
   openAdminClosingDateForm: () => set({ isVisibleAdminClosingDateForm: true }),
   closeAdminClosingDateForm: () => set({ isVisibleAdminClosingDateForm: false }),
-  setAdminClosingDateFormDate: (date) => set({ adminClosingDateFormDate: date }),
 })); 
