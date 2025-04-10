f50902a0262322dce5373c584a2a543e7ca7be5c
HakuryuSato <r700540@gmail.com>
2025-04-10 15:06:47 +0900

    ファイル名の更新

---
+++ src/hooks/admin/useAdminAttendanceViewClosingDate.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import useSWR from 'swr';
+import { useAdminAttendanceViewStore } from "@/stores/admin/adminAttendanceViewSlice";
+import { fetchSetting } from "@/utils/client/apiClient";
+
+export const useAdminAttendanceViewClosingDate = () => {
+  const setAdminAttendanceViewClosingDate = useAdminAttendanceViewStore(
+    (state) => state.setAdminAttendanceViewClosingDate
+  );
+
+  const { data, error, mutate } = useSWR(
+    'settings/closing-date',
+    () => fetchSetting('closing-date')
+  );
+
+  useEffect(() => {
+    if (data) {
+      setAdminAttendanceViewClosingDate(parseInt(data, 10));
+    }
+  }, [data, setAdminAttendanceViewClosingDate]);
+
+  return { closingDate: data, error, mutateClosingDate: mutate };
+}; 
