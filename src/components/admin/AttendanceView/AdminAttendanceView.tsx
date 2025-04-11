49eb51c7326493e0d936f236fe6fff64371f92c3
HakuryuSato <r700540@gmail.com>
2025-04-11 13:30:49 +0900

    AttendanceViewコンポーネントで締日や日付範囲の設定ロジックを追加

--- src/components/admin/AttendanceView/AdminAttendanceView.tsx
+++ src/components/admin/AttendanceView/AdminAttendanceView.tsx
@@ -5,10 +5,16 @@ import { AttendanceTablePersonal } from "./AttendanceTablePersonal";
 import { useAdminAttendanceView } from "@/hooks/admin/AttendanceView/useAdminAttendanceView";
 import { useAttendanceHolidays } from "@/hooks/admin/AttendanceView/useAttendanceHolidays";
 import { useAdminAttendanceViewClosingDate } from "@/hooks/admin/AttendanceView/useAdminAttendanceViewClosingDate";
+import { useAdminAttendanceViewDateRange } from "@/hooks/admin/AttendanceView/useAdminAttendanceViewDateRange";
 
 export const AdminAttendanceView: React.FC = () => {
+    // 締め日を取得
     useAdminAttendanceViewClosingDate();
+    // 締め日から日付範囲を設定
+    useAdminAttendanceViewDateRange();
+    // 日付範囲から出退勤データを取得
     useAdminAttendanceView();
+    // 祝日データを取得
     useAttendanceHolidays();
 
     const adminHomeMode = useAdminHomeStore((state) => state.adminHomeMode);
